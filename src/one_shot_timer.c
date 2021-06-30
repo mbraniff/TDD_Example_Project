a8718ff934936be51af3056a0253bc7f33bfdfec
mbraniff <mbraniff@rocketmail.com>
2021-06-30 10:04:30 -0500

    Not being used

--- src/one_shot_timer.c
+++ src/one_shot_timer.c
@@ -1,1 +1,21 @@
 #include "one_shot_timer.h"
+
+static void callback(one_shot_timer_t* timer){
+    timer->callback(timer->context);
+}
+
+void one_shot_timer_init(one_shot_timer_t* timer, uint8_t stop, callback_t callback, void* context)
+{
+    timer->stop_time = stop;
+    timer->callback = callback;
+    timer->context = context;
+}
+void one_shot_timer_set_callback(one_shot_timer_t* timer, callback_t callback);
+void one_shot_timer_set_context(one_shot_timer_t* timer, void* context);
+void one_shot_timer_time_did_change(one_shot_timer_t* timer)
+{
+    if(timer->current_time >= timer->stop_time)
+    {
+        callback(timer);
+    }
+}
--- test/test_one_shot_timer.c
+++ test/test_one_shot_timer.c
@@ -1,5 +1,3 @@
-#ifdef TEST
-
 #include "unity.h"
 
 #include "one_shot_timer.h"
@@ -16,5 +14,3 @@ void test_one_shot_timer_NeedToImplement(void)
 {
     TEST_IGNORE_MESSAGE("Need to Implement one_shot_timer");
 }
-
-#endif // TEST
